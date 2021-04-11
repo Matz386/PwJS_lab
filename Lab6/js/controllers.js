d22a1519eb5c70b056dfbcbd97b34bb946e91c49
Matz386 <79979832+Matz386@users.noreply.github.com>
2021-04-11 14:46:53 +0200

    Add files via upload

---
+++ Lab6/js/controllers.js
@@ -0,0 +1,33 @@
+var portfolioApp = angular.module('portfolioApp',[]);
+
+portfolioApp.controller('GalleryListCtrl', function($scope)
+{
+    $scope.galleries = 
+    [
+        { 'title':'Mustang Super Snake',
+        'when':'2020',
+        'thumbnailUrl':'img/Mustang Shelby Super Snake.jpg'
+        },
+        { 'title':'Mustang Shelby GT500',
+        'when':'2013',
+        'thumbnailUrl':'img/Mustang Shelby GT500 2013.jpg'
+        },
+        { 'title':'Mustang Classic',
+        'when':'1968',
+        'thumbnailUrl':'img/Mustang Classic.jpg'
+        },
+        { 'title':'Mustang Shelby GT500',
+        'when':'2021',
+        'thumbnailUrl':'img/Mustang Shelby GT500 2021.jpg'
+        },
+        { 'title':'Mustang Ghia',
+        'when':'1980',
+        'thumbnailUrl':'img/Mustang 1980.jpg'
+        },
+        { 'title':'Mustang Cobra',
+        'when':'1996',
+        'thumbnailUrl':'img/Mustang 1996.jpeg'
+        }
+    ];
+    $scope.galleries.length;
+});
